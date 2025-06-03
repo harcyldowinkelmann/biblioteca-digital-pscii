ef95d6c6020d15335e552b377f8c3f983c8da3d3
harcyldoleonardo <134642657+harcyldowinkelmann@users.noreply.github.com>
2025-06-02 20:48:15 -0400

    feat(frontend): Adicionando icons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+    <v-app>
+      <v-main>
+        <v-container>
+          <v-btn>
+            <v-icon
+      color="green-darken-2"
+      icon="mdi-domain"
+      size="large"
+    ></v-icon>
+
+    <v-icon
+      color="blue-darken-2"
+      icon="mdi-message-text"
+      size="large"
+    ></v-icon>
+
+    <v-icon
+      color="purple-darken-2"
+      icon="mdi-dialpad"
+      size="large"
+    ></v-icon>
+
+    <v-icon
+      color="teal-darken-2"
+      icon="mdi-email"
+      size="large"
+    ></v-icon>
+
+    <v-icon
+      color="blue-grey-darken-2"
+      icon="mdi-call-split"
+      size="large"
+    ></v-icon>
+
+    <v-icon
+      color="orange-darken-2"
+      icon="mdi-arrow-up-bold-box-outline"
+      size="large"
+    ></v-icon>
+            Botão Vuetify 09
+          </v-btn>
+        </v-container>
+      </v-main>
+    </v-app>
+  </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
   }
-}
 </script>
 
 <style>
--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -5,9 +5,23 @@
 
 // export default new Vuetify({});
 
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+// import '@mdi/font/css/materialdesignicons.css'
+// import 'vuetify/styles'
 
-const vuetify = createVuetify({
-  ssr: true,
+// const vuetify = createVuetify({
+//   ssr: true,
+// })
+
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
+
