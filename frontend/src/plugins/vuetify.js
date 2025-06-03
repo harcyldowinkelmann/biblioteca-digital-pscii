ea278bd835494d1197cef4bd9f012cc99afab082
harcyldoleonardo <134642657+harcyldowinkelmann@users.noreply.github.com>
2025-06-02 20:27:43 -0400

    Adicionando Vuetify

---
+++ frontend/src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+// import Vue from 'vue';
+// import Vuetify from 'vuetify/lib';
+
+// Vue.use(Vuetify);
+
+// export default new Vuetify({});
+
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+const vuetify = createVuetify({
+  ssr: true,
+})
