fb20431a5efadc1278ddccab2b4a9b5f894ef98b
harcyldoleonardo <134642657+harcyldowinkelmann@users.noreply.github.com>
2025-06-23 20:36:33 -0400

    feat(Cadastro): Nova tela de cadastro

--- frontend/src/views/Cadastro.vue
+++ frontend/src/views/Cadastro.vue
@@ -1,7 +1,58 @@
 <template>
-	<v-container class="d-flex justify-center border">
-		<v-card class="ma-7 pa-5" style="width: 80%; height: 700px; background-color: #3a6391;" elevation="0">
-			
+	<v-container class="d-flex justify-center">
+		<v-card class="border ma-7 pa-5" style="width: 75%; height: 700px; background-color: #3a6391;" elevation="0">
+			<v-row>
+				<v-sheet class="pa-4 text-white text-h4 text-left" elevation="0" style="background-color: transparent; font-weight: bolder;">
+					CADASTRE-SE
+				</v-sheet>
+			</v-row>
+
+			<v-row class="mt-8">
+				<v-col cols="6">
+					<v-row>
+						<v-col cols="12" class="text-left"><label>Nome:</label></v-col>
+						<v-col cols="12"><input type="text" class="custom-input w-100" /></v-col>
+					</v-row>
+				</v-col>
+				<v-col cols="6">
+					<v-row>
+						<v-col cols="12" class="text-left"><label>Instituição de Ensino:</label></v-col>
+						<v-col cols="12"><input type="text" class="custom-input w-100" /></v-col>
+					</v-row>
+				</v-col>
+			</v-row>
+
+			<v-row class="mt-8">
+				<v-col cols="6">
+					<v-row>
+						<v-col cols="12" class="text-left"><label>Email:</label></v-col>
+						<v-col cols="12"><input type="text" class="custom-input w-100" /></v-col>
+					</v-row>
+				</v-col>
+				<v-col cols="6">
+					<v-row>
+						<v-col cols="12" class="text-left"><label>Senha:</label></v-col>
+						<v-col cols="12"><input type="password" class="custom-input w-100" /></v-col>
+					</v-row>
+				</v-col>
+			</v-row>
+
+			<v-row class="mt-8">
+				<v-col cols="6">
+					<v-row>
+						<v-col cols="12" class="text-left"><label>Eu sou:</label></v-col>
+						<v-col cols="12"><input type="text" class="custom-input w-100" /></v-col>
+					</v-row>
+				</v-col>
+				<v-col cols="6">
+					<v-row>
+						<v-col cols="12"><label></label></v-col>
+						<v-col cols="12" class="text-left">
+							<v-btn size="large" class="login-button mx-auto" elevation="0">Cadastrar-me</v-btn>
+						</v-col>
+					</v-row>
+				</v-col>
+			</v-row>
 		</v-card>
 	</v-container>
 </template>
@@ -23,7 +74,39 @@ export default {
 </script>
 
 <style>
+	label {
+		color: white;
+		font-weight: bolder;
+		padding-left: 10px;
+	}
+
 	.border {
 		border: 1px solid white;
+	}
+
+	.custom-input {
+		border: 2px solid black;
+		border-radius: 25px;
+		padding: 8px 12px;
+		font-size: 16px;
+		color: #000;
+		background-color: white;
+	}
+
+	.login-button {
+		background-color: #00B8D4; /* azul-esmeralda */
+		color: white;
+		padding: 10px 24px;
+		border-radius: 25px;
+		text-transform: none; /* evita letras maiúsculas automáticas */
+		font-size: 16px;
+	}
+
+	.login-button:hover {
+		background-color: #00B8D4; /* tom mais escuro no hover */
+	}
+
+	.space-v-row {
+		margin-top: 25px;
 	}
 </style>
